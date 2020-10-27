1f01fa3f3b0ca022e6ffa3aad3a1af45c2feada9
FUKUDA Shoma <fkshom@gmail.com>
2020-10-27 22:55:50 +0900

    add install ghq script

---
+++ bin/install_ghq.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+filename=ghq_linux_amd64.zip
+curl -Lo /tmp/$filename https://github.com/x-motemen/ghq/releases/latest/download/ghq_linux_amd64.zip
+
+mkdir -p ~/.local/bin
+unzip -p /tmp/$filename ghq_linux_amd64/ghq > ~/.local/bin/ghq
+chmod a+x ~/.local/bin/ghq
+
+mkdir -p ~/.local/share/bash-completions
+unzip -p /tmp/$filename ghq_linux_amd64/misc/bash/_ghq > ~/.local/share/bash-completions/_ghq
+
+mkdir -p ~/.local/share/zsh-completions/
+unzip -p /tmp/$filename ghq_linux_amd64/misc/zsh/_ghq > ~/.local/share/zsh-completions/_ghq
+
+rm /tmp/$filename
+
