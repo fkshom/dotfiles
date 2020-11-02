d1f414bd06679943ab8d5ea601db07dbacb30004
FUKUDA Shoma <fkshom@gmail.com>
2020-11-03 01:00:09 +0900

    fix gwt.sh

--- sh.d/gwt.sh
+++ sh.d/gwt.sh
@@ -3,6 +3,33 @@
 GWT_DIR=${GWT_DIR:-~/.git-worktrees/}
 GWT_REPOS_DIR=${GWT_REPOS_DIR:-~/.ghq/}
 
+function set-filter() {
+  previewer="cat"
+  if type "batcat" >/dev/null 2>&1; then
+    previewer='batcat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*'
+  fi
+  filters="fzf-tmux:fzf:peco:percol:gof"
+  while [[ -n $filters ]]; do
+    filter=${filters%%:*}
+    if type "$filter" >/dev/null 2>&1; then
+      [[ "$filter" = "fzf" ]] \
+        && filter=($filter --preview "$previewer") \
+        && multi_filter=($filter --multi --ansi --prompt="gwt >")
+      [[ "$filter" = "fzf-tmux" ]] \
+        && filter=($filter --preview "$previewer") \
+        && multi_filter=($filter --multi --ansi --prompt="gwt >")
+      #[[ "$filter" = "fzf" ]] && filter=($filter --ansi --prompt="gwt >") && multi_filter=($filter --multi --ansi --prompt="gwt >")
+      #[[ "$filter" = "fzf-tmux" ]] && filter=($filter -r --ansi --prompt="gwt >") && multi_filter=($filter --multi --ansi --prompt="gwt >")
+      return 0
+    else
+      filters="${filters#*:}"
+    fi
+  done
+  echo "gwt: gwt requires fuzzy finder. Either of these is necessary." 1>&2
+  echo "fzf, fzf-tmux, peco, percol, gof" 1>&2
+  exit 1
+}
+
 gwt-checkout(){
   commit_ish=$1
   branchname=${commit_ish##*/}
@@ -52,7 +79,7 @@ peco-gwt-cd(){
   fi
 }
 
-peco-gwt-cd(){
+gwt-cd(){
   if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
       echo "Here is not inside git work tree"
       return
@@ -61,7 +88,7 @@ peco-gwt-cd(){
   GIT_TOPLEVEL_DIR=$(git rev-parse --show-toplevel)
   GIT_SUPERROOT_DIR=$(r=$(git rev-parse --git-dir) && r=$(cd "$r" && pwd)/ && echo "${r%%/.git/*}")
   branches=$(git branch --all | grep -v HEAD | cut -b 3- )
-  commit_ish=$(echo "$branches" | fzf )
+  commit_ish=$(echo "$branches" | "${filter[@]}")
   branchname=${commit_ish##*/}
   if [ -n "$branchname" ]; then
       cmd="git worktree add -b $branchname ${GIT_SUPERROOT_DIR}/worktrees/$branchname $commit_ish"
@@ -73,27 +100,20 @@ peco-gwt-cd(){
   fi
 }
 bind -r "\C-]"
-bind -x '"\C-]p":peco-gwt-cd'
+bind -x '"\C-]p":gwt-cd'
 
 
-#ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*"
-#ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'"
-
-ghq-fzf(){
+gwt-ghq(){
+  set-filter
   local src
-  src=$(ghq list | fzf --preview "batcat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+  echo "${filter[@]}"
+  src=$(ghq list | "${filter[@]}")
   if [ -n "$src" ]; then
     local l="cd $(ghq root)/$src"
     READLINE_LINE="$l"
     READLINE_POINT=${#l}
   fi
 }
-#cdgwt(){
-#    branches=$(git branch -vv) &&
-#    branch=$(echo "$branches" | fzf +m) &&
-#    git worktree 
-#
-#}
-bind -x '"\C-]g":ghq-fzf'
+bind -x '"\C-]g":gwt-ghq'
 
 
