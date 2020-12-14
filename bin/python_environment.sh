0503039c486e95dce0aa402384b6b9e3b0fb822f
FUKUDA Shoma <fkshom@gmail.com>
2020-12-14 15:42:08 +0900

    fix poetry path

--- bin/python_environment.sh
+++ bin/python_environment.sh
@@ -10,7 +10,7 @@ if [ -d $HOME/.pyenv ]; then
 else
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
   curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
-  poetry config virtualenvs.in-project true
+  $HOME/.poetry/bin/poetry config virtualenvs.in-project true
 fi
 
 echo "Finished"
