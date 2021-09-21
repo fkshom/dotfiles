e1b538fdad0e8233a4b43595afc8dd2a144b6a47
Shoma FUKUDA <fkshom@gmail.com>
2021-09-21 21:14:19 +0900

    fix python installer

--- bin/install_python_environment.sh
+++ bin/install_python_environment.sh
@@ -3,12 +3,15 @@
 echo "Install pyenv"
 
 sudo apt update
-sudo apt install -y build-essential libreadline-dev zlib1g-dev libsqlite3-dev libbz2-dev libssl-dev libffi-dev curl
+sudo apt install -y build-essential libreadline-dev zlib1g-dev libsqlite3-dev libbz2-dev libssl-dev libffi-dev curl python3-distutils
 
-if [ -d $HOME/.pyenv ]; then
-  (cd $HOME/.pyenv; git pull)
-else
+if [ ! -d $HOME/.pyenv ]; then
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+else
+  (cd $HOME/.pyenv; git pull)
+fi
+
+if [ ! -d $HOME/.poetry ]; then
   curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
   $HOME/.poetry/bin/poetry config virtualenvs.in-project true
 fi
