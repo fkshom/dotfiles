411b376f2b3e1dd882f59a196f66e6e1dbae664e
user <user@example.com>
2022-12-16 01:19:46 +0900

    fix nvm installer

--- bin/install_nodejs_environment.sh
+++ bin/install_nodejs_environment.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export NVM_DIR="$HOME/.nvm"
-git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+git clone --depth=1 https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 cd "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+source nvm.sh
+nvm install --lts
 
