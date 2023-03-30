8aadb99a53421058e00740e0822ad727fce9b1e5
Shoma FUKUDA <fkshom@gmail.com>
2023-03-30 18:03:55 +0900

    fix install_microk8s.sh

--- bin/install_microk8s.sh
+++ bin/install_microk8s.sh
@@ -3,7 +3,11 @@
 sudo snap install microk8s --classic
 sudo snap alias microk8s.kubectl kubectl
 microk8s.enable dns
+microk8s.enable dashboard
 microk8s.enable storage
+#microk8s.enable metallb
+
+#microk8s kubectl port-forward -n kube-system service/kubernetes-dashboard --address 0.0.0.0 10443:443
 
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
