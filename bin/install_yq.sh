1d4cf2bef3d50d3665e8c03b19c5227acb8dcf1f
Shoma FUKUDA <fkshom@gmail.com>
2023-04-18 16:41:34 +0900

    fix yq installer to use latest

--- bin/install_yq.sh
+++ bin/install_yq.sh
@@ -4,6 +4,6 @@ VERSION=v4.23.1
 BINARY=yq_linux_amd64
 
 mkdir -p ~/.local/bin
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O ~/.local/bin/yq &&\
+wget https://github.com/mikefarah/yq/releases/latest/download/${BINARY} -O ~/.local/bin/yq &&\
     chmod +x ~/.local/bin/yq
 
