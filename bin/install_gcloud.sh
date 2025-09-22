f734337c7be4adf8760e88c73fb9ca3a5d252fe7
Shoma FUKUDA <fkshom@gmail.com>
2025-09-23 02:35:26 +0900

    add gcloud installer

---
+++ bin/install_gcloud.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates gnupg curl
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+sudo apt-get update && sudo apt-get -y install google-cloud-cli
+
+echo "-------------------------------"
+echo "must run 'gcloud init' to finish setup"
+echo "-------------------------------"
